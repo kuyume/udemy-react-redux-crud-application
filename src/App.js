0a48134e80057c1695312e294abb4460980982d3
Yusaku Shibata <gnorexec@gmail.com>
2020-12-31 01:43:28 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 
 function App() {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName" }
+  ]
   return (
     <>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </>
   )
   ;
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
